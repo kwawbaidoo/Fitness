3b7d555b6f39fda11481cfec49bd62da9c29e22a
kwawbaidoo <kwaw.baidoo@gmail.com>
2024-05-23 00:28:20 +0000

    fitness header

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        lato:['lato'],
+        inter:['inter']
+      },
+      colors:{
+        Fuchsia: '#BA24D5'
+      }
+    },
+  },
+  plugins: [],
+}
+
