91c665fe060b80593f56fb7a115d7b9aff71e268
kwawbaidoo <kwaw.baidoo@gmail.com>
2024-05-27 21:51:10 +0000

    worked on active class style for navigation list

--- script/script.js
+++ script/script.js
@@ -1,10 +1,8 @@
-const navItems = document.querySelectorAll('.nav-item');
+const navItems = document.querySelectorAll(".nav-item");
 console.log(navItems);
-navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        // Remove active class from all items
-        navItems.forEach(nav => nav.classList.remove('text-black'));
-        // Add active class to the clicked item
-        item.classList.add('text-black');
-    });
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navItems.forEach((nav) => nav.classList.remove("text-black"));
+    item.classList.add("text-black");
+  });
 });
